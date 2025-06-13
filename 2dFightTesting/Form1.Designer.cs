8e81fe0301b9e94368f6935e70e8c5acd0da412e
AyusPate412 <159576227+AyusPate412@users.noreply.github.com>
2025-06-13 17:44:30 -0400

    background and created gamescreen.cs nathan move the stuff from form1.cs to gamescreen

--- 2dFightTesting/Form1.Designer.cs
+++ 2dFightTesting/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.roundEndTimer = new System.Windows.Forms.Timer(this.components);
@@ -59,6 +60,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(684, 411);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
