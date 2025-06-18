90e0548166dd9d73453bd199eb92e6943b4bcd3f
NEhnes <162652120+NEhnes@users.noreply.github.com>
2025-06-17 21:16:40 -0400

    xml reading done, need sorting, need writing and leaderboard screen

--- 2dFightTesting/WinScreen.cs
+++ 2dFightTesting/WinScreen.cs
@@ -7,22 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace _2dFightTesting
 {
     public partial class WinScreen : UserControl
     {
+        List<Player> players = new List<Player>();
+
+        class Player
+        {
+            public string name;
+            public string wins;
+        }
         public WinScreen(string _winnerName)
         {
             InitializeComponent();
 
             winnerLabel.Text = $"{_winnerName} wins!!!";
 
-            // check xml for name
+            LoadStats();
+
+            winnerLabel.Text = players[2].wins;
+
+            CheckListForWinner();
 
             // if exists, update score
 
             // if doe snot exist, create new element with name and score
+        }
+
+        private void LoadStats()
+        {
+            XmlReader reader = XmlReader.Create("Resources/WinRecords.xml", null);
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    Player p = new Player();
+
+                    p.name = reader.GetAttribute("name");
+                    p.wins = reader.GetAttribute("wins");
+
+                    players.Add(p);
+                }
+            }
+        }
+
+        private void CheckListForWinner()
+        {
         }
 
         private void leaderboard_Click(object sender, EventArgs e)
